b369971f286bd96f906a163cb537883a6fa99b9c
Suleiman <real1sule@gmail.com>
2025-10-12 16:24:37 +0300

    not yet completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Links from "./Links"; // assuming you have this
 
-function About() {
+function About({ bio, links }) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+
+      {/* only render <p> if bio is not empty */}
+      {bio && <p>{bio}</p>}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+
+      {/* render the Links component */}
+      <Links github={links.github} linkedIn={links.linkedIn} />
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,33 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
+import Links from "./links";
+import links from './data'
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
 
 function App() {
+  const github = "https://github.com/liza"
+  const linkedin = "https://www.linkedin.com/in/liza/"
+  const bio = 'I made this'
   return (
     <div>
       <NavBar />
       <Home />
-      <About />
+      <About bio = {bio} />
+      <Links github={links.github} linkedIn={links.linkedin}/>
     </div>
   );
 }
+
+function user (props) {
+  return (
+    <div>
+      {user.props}
+    </div>
+  );
+}
+
 
 export default App;
